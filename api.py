c6f6e57ace12f19498b3526154e09cb9ffa8a448
Abhishek S P <abhisheksp1993@gmail.com>
2018-09-15 10:34:11 -0500

    Add POST endpoint to update offers for a user

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
-import flask
-from offers import update_offers
+from flask import Flask, request, jsonify
+from offers import update_user_offers
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
-def home():
-    update_offers()
-    return "<h1>Offers Updated!</p>"
+
+@app.route('/update/offers', methods=['POST'])
+def update_offers_handler():
+    request_body = request.get_json()
+    user_id = request_body['user_id']
+    emails = request_body['emails']
+    update_user_offers(user_id, emails)
+    return jsonify({'offers_updated': True})
 
 app.run()
--- offers.py
+++ offers.py
@@ -9,52 +9,10 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ricehack2018.firebaseio.com/'
 })
 
+
 # TODO: possible improvements
 # 1. add appropriate index
 # 2. trigger asynchronously
-
-
-# TODO: remove after integrating with other APIs
-def seed_emails():
-    user_id = 'abhisheksp1993'
-
-    aldo_email = '''
-    ALDO Offer
-
-    Free Shipping!
-
-    {ALDO_PRODUCTS}
-
-    Valid until October 19th, 2018.
-    '''
-
-    bestbuy_email = '''
-    BestBuy Offer
-
-    Best deals in the world!
-
-    {BestBuy_PRODUCTS}
-
-    Valid until September 29th, 2018.
-    '''
-
-    ref = db.reference('/')
-    ref.set({
-        'emails': {
-            'users': {
-                user_id: [
-                    aldo_email,
-                    bestbuy_email,
-                ]
-            }
-        },
-        'offers': {
-            'users': {
-                user_id: [],
-            },
-        }
-    })
-
 
 def parse_email(email_content):
     aldo_offer = {
@@ -82,24 +40,52 @@ def parse_email(email_content):
         return bestbuy_offer
 
 
-def parse_emails():
-    ref = db.reference('/emails/users')
-    users_emails = ref.get()
-    user_offers = {}
-    for user_id, emails in users_emails.items():
-        user_offers[user_id] = list(map(parse_email, emails))
+def parse_emails(emails):
+    user_offers = list(map(parse_email, emails))
     return user_offers
 
 
-def save_user_offers(user_offers):
-    for user_id, new_offers in user_offers.items():
-        ref = db.reference('/offers/users/')
-        existing_offers = ref.child(user_id).get() or []
-        existing_offers += new_offers  # TODO: find a better way to add to existing list
-        ref.update({user_id: existing_offers})
+def save_new_user_offers(user_id, new_offers):
+    ref = db.reference('/offers/users/')
+    existing_offers = ref.child(user_id).get() or []
+    existing_offers += new_offers  # TODO: find a better way to add to existing list
+    ref.update({user_id: existing_offers})
 
 
-def update_offers():
-    seed_emails()
-    user_offers = parse_emails()
-    save_user_offers(user_offers)
+def update_user_offers(user_id, emails):
+    new_offers = parse_emails(emails)
+    save_new_user_offers(user_id, new_offers)
+
+
+# TEST
+def test_runner():
+    aldo_email = '''
+    ALDO Offer
+
+    Free Shipping!
+
+    {ALDO_PRODUCTS}
+
+    Valid until October 19th, 2018.
+    '''
+    bestbuy_email = '''
+    BestBuy Offer
+
+    Best deals in the world!
+
+    {BestBuy_PRODUCTS}
+
+    Valid until September 29th, 2018.
+    '''
+    user_id = 'abhisheksp1993'
+    emails = [aldo_email, bestbuy_email]
+    update_user_offers(user_id, emails)
+
+
+def reset_offers(user_id):
+    ref = db.reference('/offers/users/{}'.format(user_id))
+    ref.set({})
+
+
+# reset_offers('abhisheksp1993')
+# test_runner()
